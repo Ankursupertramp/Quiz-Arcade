129c9e2dd8151a3c42d8b0ed5d73c16877e09a72
Ankur <107772408+Ankursupertramp@users.noreply.github.com>
2022-12-13 12:06:39 +0530

    Update script.js
    Good

--- script.js
+++ script.js
@@ -1,51 +1,98 @@
-const quizData =[
-   {
-   question: "Popular GTA games like GTA5 and GTA SA contain a parody version of which fashion magazine?",
-   a: "Cosmopolitan",
-   b : "Vogue",
-   c : "ELLE",
-   d: "W",
-   correct : "b",
-   },
 
+    const quizData =[
    {
-   question : "Which video game antagonist character became a meme on Tiktok in 2021 due to its resemblance with a girl's face?",
+      question: "Popular GTA games like GTA5 and GTA SA contain a parody version of which fashion magazine?",
+      a: "Cosmopolitan",
+      b : "Vogue",
+      c : "ELLE",
+      d: "W",
+      correct : "b",
+      },
    
-   a : "Lance Vance",
-   b : "Kefka",
-   c : "Bloody Mary",
-   d : "Thwomp",
-   correct : "d"
-   
-   },
-   {
-   question : "This rumored character in Minecraft looks like Steve, roams around forests and is known for spooking players out. Who is this spooky guy?",
-   
-   a : "Herobrine",
-   b : "Barbarian King",
-   c : "EnderWorm",
-   d : "Shulker",
-   correct : "a"
-   },
-   {
-      question : "Name the special spell in Clash of Clans released in Christmas of 2012.",
+      {
+      question : "Which video game antagonist became a meme on Tiktok in 2021 due to its resemblance with a girl's face?",
       
-      a : "Santa Spell",
-      b : "X-Mas Spell",
-      c : "Ultra-freeze Spell",
-      d : "Snow Spell",
+      a : "Lance Vance",
+      b : "Kefka",
+      c : "Bloody Mary",
+      d : "Thwomp",
+      correct : "d"
+      
+      },
+      {
+      question : "This rumored character in Minecraft looks like Steve and is known for spooking players out. Who is this spooky guy?",
+      
+      a : "Herobrine",
+      b : "Barbarian King",
+      c : "EnderWorm",
+      d : "Shulker",
       correct : "a"
       },
-  {
-      question : "Name the special spell in Clash of Clans released in Christmas of 2012.",
+      {
+       question : "Name the special spell in Clash of Clans released in Christmas of 2012.",
          
          a : "Santa Spell",
          b : "X-Mas Spell",
          c : "Ultra-freeze Spell",
          d : "Snow Spell",
          correct : "a"
-   }
-   ] ;
+         },
+     {
+      question : "Name the main character in Human Fall Flat.",
+            
+            a : "Rob",
+            b : "Bro",
+            c : "Bob",
+            d : "Ally",
+            correct : "c"
+      },
+      {
+         question : "Which roblox game makes the most money as of 2022?",
+               
+               a : "Livetopia",
+               b : "Work at a Pizza Place",
+               c : "Adopt Me",
+               d : "Paintball",
+               correct : "c"
+         },
+         {
+            question : "Alien Party is only unlocked at Level __ of Science Career in SIMS 4.",
+                  
+                  a : "7",
+                  b : "10",
+                  c : "15",
+                  d : "25",
+                  correct : "b"
+            },
+         {
+             question : "Which A-List celebrity voiced for Call of Duty:Ghosts?",
+                     
+                     a : "Mariah Carey",
+                     b : "Brandon Routh",
+                     c : "Robert Pattinson",
+                     d : "Ariana Grande",
+                     correct : "b"
+          },
+          {
+            question : "How many awards did Candy Crush Saga get till date?",
+                    
+                    a : "4",
+                    b : "7",
+                    c : "2",
+                    d : "5",
+                    correct : "c"
+         },
+         {
+            question : "Name the first limited character ever created in Subway Surfers?",
+                    
+                    a : "Zombie Jake",
+                    b : "Santa Jake",
+                    c : "Rockstar Jake",
+                    d : "Yeti Jake",
+                    correct : "a"
+         }
+   
+      ] ;
    
    const quiz = document.getElementById('quiz')
    const answerEls = document.querySelectorAll('.answer')
